8a30797f19f84bfec67d9e208f95dfe29544a3d3
skpanagiotis <panagiotis.skias@gmail.com>
2023-03-11 23:14:57 +0200

    Update shell appRouting

--- shell/src/app/app-routing.module.ts
+++ shell/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: FormComponent },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        remoteName: 'projectOne',
+        exposedModule: 'DashboardModule',
+      }).then((m) => {
+        return m.DashboardModule;
+      }),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
